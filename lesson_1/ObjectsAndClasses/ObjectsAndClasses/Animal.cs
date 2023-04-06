b1142f76ca537b8ff5f15bbba69ee7143e1273ba
Vladyslav Ishchuk <vladyslav.ishchuk@gmail.com>
2023-04-06 17:52:04 +0300

    Lesson 1 test commit

---
+++ lesson_1/ObjectsAndClasses/ObjectsAndClasses/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsAndClasses
+{
+
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public abstract void MakeSound(); 
+    }
+
+    public enum Size
+    {
+        Small, Medium, SmallLarge,
+    }
+
+    public class Dog : Animal
+    {
+        public Size DogSize { get; set; }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    public class Pug : Dog
+    {
+        public Pug()
+        {
+            DogSize = Size.Small;
+        }
+    }
+}
